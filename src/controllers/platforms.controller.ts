abd9adc50aa916d3e45213e642efa20cac629552
Beaucourt89 <beaucourt.pro@gmail.com>
2020-10-16 11:20:35 +0200

    Video-Game avec le CRUD complet

--- src/controllers/platforms.controller.ts
+++ src/controllers/platforms.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import PlatformModel from "../models/platformModel";
+import slugify from "slug";
 
 const clientWantsJson = (request: Request): boolean => request.get("accept") === "application/json";
 
@@ -30,6 +31,52 @@ export function show(platformModel: PlatformModel) {
       } else {
         response.status(404).render("pages/not-found");
       }
+    }
+  };
+}
+
+export function create(platformModel: PlatformModel) {
+  return async (request: Request, response: Response): Promise<void> => {
+    const platformInput = { ...request.body, slug: slugify(request.body.name) };
+    const errors = platformModel.validate(platformInput);
+    if (errors.length > 0) {
+      response.status(400).json({ errors });
+    } else {
+      const platform = await platformModel.insertOne(platformInput);
+      response.status(201).json(platform);
+    }
+  };
+}
+
+export function update(platformModel: PlatformModel) {
+  return async (request: Request, response: Response): Promise<void> => {
+    const platform = await platformModel.findBySlug(request.params.slug);
+    if (platform) {
+      const errors = platformModel.validate({ ...request.body, slug: request.params.slug });
+      if (errors.length > 0) {
+        response.status(400).json({ errors });
+      } else {
+        const updatedPlatform = await platformModel.updateOne(platform._id, {
+          ...platform,
+          ...request.body,
+          _id: platform._id,
+        });
+        response.status(201).json(updatedPlatform);
+      }
+    } else {
+      response.status(404).end();
+    }
+  };
+}
+
+export function destroy(platformModel: PlatformModel) {
+  return async (request: Request, response: Response): Promise<void> => {
+    const platform = await platformModel.findBySlug(request.params.slug);
+    if (platform) {
+      platformModel.remove(platform._id);
+      response.status(204).end();
+    } else {
+      response.status(404).end();
     }
   };
 }
--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,51 @@
 import { Db } from "mongodb";
 import * as core from "express-serve-static-core";
+import express from "express";
 import * as dotenv from "dotenv";
-import * as express from "express";
+
 import * as gamesController from "./controllers/games.controller";
 import * as nunjucks from "nunjucks";
 import * as platformsController from "./controllers/platforms.controller";
-import GameModel, { Game } from "./models/gameModel";
 import initDb from "../utils/initDatabase";
+
+import GameModel, { Game } from "./models/gameModel";
 import PlatformModel, { Platform } from "./models/platformModel";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
-const app = express();
-
-app.use("/assets", express.static("public"));
-
-nunjucks.configure("views", {
-  autoescape: true,
-  express: app,
-});
-
-app.set("view engine", "njk");
-
 const clientWantsJson = (request: express.Request): boolean => request.get("accept") === "application/json";
 
-function makeApp(db: Db): core.Express {
+const jsonParser = bodyParser.json();
+
+export function makeApp(db: Db): core.Express {
+  const app = express();
+
+  nunjucks.configure("views", {
+    autoescape: true,
+    express: app,
+  });
+
+  app.use("/assets", express.static("public"));
+  app.set("view engine", "njk");
+
   const platformModel = new PlatformModel(db.collection<Platform>("platforms"));
   const gameModel = new GameModel(db.collection<Game>("games"));
 
   app.get("/", (_request, response) => response.render("pages/home"));
 
-  // GET platforms
   app.get("/platforms", platformsController.index(platformModel));
-  // GET platforms/:slug
   app.get("/platforms/:slug", platformsController.show(platformModel));
+  app.post("/platforms", jsonParser, platformsController.create(platformModel));
+  app.put("/platforms/:slug", jsonParser, platformsController.update(platformModel));
+  app.delete("/platforms/:slug", jsonParser, platformsController.destroy(platformModel));
 
-  // GET games
+  app.get("/platforms/:slug/games", gamesController.list(gameModel));
   app.get("/games", gamesController.index(gameModel));
-  // GET platforms/:slug
   app.get("/games/:slug", gamesController.show(gameModel));
+  app.post("/games", jsonParser, gamesController.create(gameModel, platformModel));
+  app.put("/games/:slug", jsonParser, gamesController.update(gameModel));
+  app.delete("/games/:slug", jsonParser, gamesController.destroy(gameModel));
 
   app.get("/*", (request, response) => {
     if (clientWantsJson(request)) {
